d56c70c387ce8c336046b12f5988ab3a4773b3ec
Gloviano <rshafferit@gmail.com>
2021-08-24 06:52:12 +0300

    css fixes, added screenshot gallery

---
+++ content/pages/media/screenshots/index.md
@@ -0,0 +1,25 @@
+---
+title: "0 A.D. Screenshot Gallery"
+date: "2021-08-18"
+author: "0AD"
+slug: "screenshots"
+aliases: ["/media/screenshots"]
+layout: screenshots
+description: "0 A.D. Screenshot Gallery"
+draft: false
+
+resources:
+
+- src: images/BabylonParadise.jpg
+  title: Babylon Paradise
+
+- src: images/BritsAbroad.jpg
+  title: Brits Abroad
+
+- src: images/CarthaginianTown.jpg
+  title: Carthaginian Town
+
+- src: images/EgyptianPyramids.jpg
+  title: Egyptian Pyramids
+
+---
--- static/js/0ad.js
+++ static/js/0ad.js
@@ -15,3 +15,30 @@ toTopButton.addEventListener("click", () => {
     document.body.scrollTop = 0; // For Safari
     document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera  
 });
+
+
+// Lightbox/Gallery Functionality
+const lightbox = document.getElementById("lightbox");
+const lightboxContent = document.getElementById("lightbox-content");
+
+document.querySelectorAll("figure.gallery-figure > a").forEach(item => {
+    item.addEventListener("click", event => {
+        event.preventDefault();
+        let image = new Image();
+        image.src = item.href;
+        lightbox.style.display = "flex";
+
+        // Remove any existing image.
+        while (lightboxContent.firstChild) {
+            lightboxContent.removeChild(lightboxContent.firstChild);
+        }
+
+        // Add image to lightbox.
+        lightboxContent.appendChild(image);
+        console.log(image.src);
+    })
+})
+
+lightbox.addEventListener("click", function () {
+    this.style.display = "none";
+})
